1c3d606e65ac1c0b8346c92ba270543574352c72
Joel Martins <joel.martins@gmail.com>
2020-03-03 09:24:41 -0600

    first check in

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/logo.png';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image source={{uri: "https://i.imgur.com/TkIrScD.png"}} style={styles.logo}></Image> 
+      <Text style={styles.instructions}>To share a photo with a friend, just press the button below!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10,    
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+  },
+
+});
